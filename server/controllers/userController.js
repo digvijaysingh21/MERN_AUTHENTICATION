6b5dbb3d1124c0f31715978bbbc3e4e7490e0737
digvijaysingh21 <singh.v.digvijay@gmail.com>
2025-09-06 13:48:24 +0530

    improve security logig of not taking from body

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,31 +1,28 @@
 import userModel from "../models/userModel.js";
 
-export const getUserData = async(req,res) =>{
+export const getUserData = async (req, res) => {
+  try {
+    // const { userId } = req.body;
+    const userId = req.userId; // ✅ take from middleware
 
-    try{
-        const { userId } = req.body;
-
-         if (!userId) {
+    if (!userId) {
       return res.json({ success: false, message: "userId is required" });
     }
 
-        const user = await userModel.findById(userId);
+    const user = await userModel.findById(userId);
 
-        if(!user){
-            return res.json({success:false, message: 'User not found'})
-        }
-
-        res.json({
-            success: true,
-            userdata: {
-                name: user.name,
-                isAccountVerified: user.isAccountVerified,
-
-            }
-        });
-
-    }catch(error){
-        return res.json({success:false, message: error.message})
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
     }
-}
 
+    res.json({
+      success: true,
+      userdata: {
+        name: user.name,
+        isAccountVerified: user.isAccountVerified,
+      },
+    });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
