12ca12c5d063bdceb89af02a5df4e5e259b676c4
digvijaysingh21 <singh.v.digvijay@gmail.com>
2025-09-06 19:01:23 +0530

    added verify email and logout functionlaity

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
 
@@ -8,6 +8,18 @@ export const AppContextProvider = (props) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [isLoggedin, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(null);
+
+  const getAuthState = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth");
+      if (data.success) {
+        setIsLoggedIn(true);
+        getUserData();
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   const getUserData = async () => {
     try {
@@ -19,6 +31,9 @@ export const AppContextProvider = (props) => {
     }
   };
 
+  useEffect(() => {
+    getAuthState();
+  }, []);
   // const getUserData = async () => {
   //   try {
   //     const { data } = await axios.get(backendUrl + "/api/user/data", {
